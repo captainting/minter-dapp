2838318303fbe0b6ad7d1a6402e4f8ad2df3e297
Curtis ho <hohoting@gmail.com>
2022-05-15 02:41:19 +0800

    wl myself

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x819761E175269458425601EB723F55F39061e38a"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x819761E175269458425601EB723F55F39061e38a"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0x819761E175269458425601EB723F55F39061e38a"; // If you want to manually include it
