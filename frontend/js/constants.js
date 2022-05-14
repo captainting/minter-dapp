650c83e46e1abf3438cafa876d4fa38b460f566d
Curtis ho <hohoting@gmail.com>
2022-05-15 01:25:31 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xF313CC9e0605849e9d07F486E589d37b72292A47"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
